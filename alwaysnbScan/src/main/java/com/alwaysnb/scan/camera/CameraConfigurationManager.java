5c7fc9db8075f69c2207c776111429ea65146d8d
wangjc <wangjc@urwork.cn>
2019-03-19 18:44:40 +0800

    把ZxingCore核心封装jar，并引用

--- alwaysnbScan/src/main/java/com/alwaysnb/scan/camera/CameraConfigurationManager.java
+++ alwaysnbScan/src/main/java/com/alwaysnb/scan/camera/CameraConfigurationManager.java
@@ -29,7 +29,6 @@ import android.view.WindowManager;
 import com.alwaysnb.scan.FuncSwitcher;
 import com.alwaysnb.scan.camera.open.CameraFacing;
 import com.alwaysnb.scan.camera.open.OpenCamera;
-import com.google.zxing.client.android.camera.CameraConfigurationUtils;
 
 /**
  * A class which deals with reading, parsing, and setting the camera parameters which are used to
